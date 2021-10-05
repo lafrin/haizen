aee4d7ee584d71e1a59217c3ce9f2709c4fe240d
lafrin <riris.lafih.lovecat@gmail.com>
2021-10-05 21:20:44 +0900

    common.scss 追加

--- backend/public/js/app.js
+++ backend/public/js/app.js
@@ -49999,6 +49999,17 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/sass/common.scss":
+/*!************************************!*\
+  !*** ./resources/sass/common.scss ***!
+  \************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/sass/style.scss":
 /*!***********************************!*\
   !*** ./resources/sass/style.scss ***!
@@ -50011,15 +50022,16 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!*****************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/style.scss ***!
-  \*****************************************************************************************/
+/*!**********************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/style.scss ./resources/sass/common.scss ***!
+  \**********************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /home/hayashida/project/haizen/backend/resources/js/app.js */"./resources/js/app.js");
 __webpack_require__(/*! /home/hayashida/project/haizen/backend/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /home/hayashida/project/haizen/backend/resources/sass/style.scss */"./resources/sass/style.scss");
+__webpack_require__(/*! /home/hayashida/project/haizen/backend/resources/sass/style.scss */"./resources/sass/style.scss");
+module.exports = __webpack_require__(/*! /home/hayashida/project/haizen/backend/resources/sass/common.scss */"./resources/sass/common.scss");
 
 
 /***/ })

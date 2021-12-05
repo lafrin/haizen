d058b2894034bdaca5c05e5f85d8fbff70cc0fa9
lafrin <riris.lafih.lovecat@gmail.com>
2021-12-05 17:54:39 +0900

    update: const.php 0:disable => 9:disable

--- backend/config/const.php
+++ backend/config/const.php
@@ -7,14 +7,14 @@ return [
       'use'=>['num'=>2, 'jp'=>'使用'],
       // 'bill'=>['num'=>3, 'jp'=>'清算'],
       'cleaning'=>['num'=>4, 'jp'=>'清掃'],
-      'disable'=>['num'=>5, 'jp'=>'不可'],
+      'disable'=>['num'=>9, 'jp'=>'不可'],
     ],
     'NUM' => [
       '1'=>['en'=>'enable', 'jp'=>'可能'],
       '2'=>['en'=>'use', 'jp'=>'使用'],
       // '3'=>['en'=>'bill', 'jp'=>'清算],
       '4'=>['en'=>'cleaning', 'jp'=>'清掃'],
-      '0'=>['en'=>'disable', 'jp'=>'不可'],
+      '9'=>['en'=>'disable', 'jp'=>'不可'],
     ],
   ],
 
